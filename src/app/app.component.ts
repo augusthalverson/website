0fbb7e6b294e647230e91c90023d98a37c57a330
August Halverson <augusthalverson@gmail.com>
2021-02-18 09:35:56 -0600

    Something

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { AfterContentInit, Component } from '@angular/core';
-import anime from 'animejs/lib/anime.es.js';
+import anime from 'animejs';
 import Typed from 'typed.js';
 
 @Component({
